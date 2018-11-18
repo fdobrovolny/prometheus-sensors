c9ff9775b804cff93980b0aff6257641daa288e1
Pim van Pelt <pim@ipng.nl>
2018-11-18 23:49:33 +0100

    Uncrustify

--- src/barometer_drv.c
+++ src/barometer_drv.c
@@ -5,34 +5,34 @@
 #include "mgos_prometheus_sensors.h"
 #include <math.h>
 
-#define NUM_BARO 5 
+#define NUM_BARO    5
 struct mgos_barometer *s_barometer[NUM_BARO];
 
 static void barometer_prometheus_metrics(struct mg_connection *nc, void *user_data) {
   struct mgos_barometer_stats stats;
-  float temperature=NAN, humidity=NAN, pressure=NAN;
+  float temperature = NAN, humidity = NAN, pressure = NAN;
 
-  for (int i=0; i<NUM_BARO && s_barometer[i]; i++) {
+  for (int i = 0; i < NUM_BARO && s_barometer[i]; i++) {
     const char *type = mgos_barometer_get_device_name(s_barometer[i]);
 
     if (mgos_barometer_get_temperature(s_barometer[i], &temperature)) {
       mgos_prometheus_metrics_printf(nc, GAUGE,
-                                   "temperature", "Temperature in Celcius",
-                                   "{sensor=\"%d\",type=\"%s\"} %f", i, type, temperature);
+                                     "temperature", "Temperature in Celcius",
+                                     "{sensor=\"%d\",type=\"%s\"} %f", i, type, temperature);
     }
-  
+
     if (mgos_barometer_get_pressure(s_barometer[i], &pressure)) {
       mgos_prometheus_metrics_printf(nc, GAUGE,
-                                   "pressure", "Barometer pressure in Pascals",
-                                   "{sensor=\"%d\", type=\"%s\"} %f", i, type, pressure);
+                                     "pressure", "Barometer pressure in Pascals",
+                                     "{sensor=\"%d\", type=\"%s\"} %f", i, type, pressure);
     }
-  
+
     if (mgos_barometer_get_humidity(s_barometer[i], &humidity)) {
       mgos_prometheus_metrics_printf(nc, GAUGE,
-                                   "humidity", "Relative humidity percentage",
-                                   "{sensor=\"%d\",type=\"%s\"} %f", i, type, humidity);
+                                     "humidity", "Relative humidity percentage",
+                                     "{sensor=\"%d\",type=\"%s\"} %f", i, type, humidity);
     }
-  
+
     if (mgos_barometer_get_stats(s_barometer[i], &stats)) {
       mgos_prometheus_metrics_printf(nc, COUNTER,
                                      "sensor_read_total", "Total reads from sensor",
@@ -59,9 +59,9 @@ static void barometer_prometheus_metrics(struct mg_connection *nc, void *user_da
 
 static void barometer_timer_cb(void *user_data) {
   struct mgos_barometer *baro = (struct mgos_barometer *)user_data;
-  const char *type = mgos_barometer_get_device_name(baro);
+  const char *           type = mgos_barometer_get_device_name(baro);
 
-  float temperature=NAN, humidity=NAN, pressure=NAN;
+  float temperature = NAN, humidity = NAN, pressure = NAN;
   struct mgos_barometer_stats stats_before, stats_after;
   uint32_t usecs = 0;
 
@@ -79,30 +79,38 @@ static void barometer_timer_cb(void *user_data) {
 
 static struct mgos_barometer *barometer_create(struct mgos_i2c *i2c, uint8_t i2caddr, enum mgos_barometer_type type) {
   struct mgos_barometer *b;
-  b=mgos_barometer_create_i2c(i2c, i2caddr, type);
-  if (!b) return NULL;
+
+  b = mgos_barometer_create_i2c(i2c, i2caddr, type);
+  if (!b) {
+    return NULL;
+  }
   mgos_barometer_set_cache_ttl(b, 1000);
   mgos_set_timer(mgos_sys_config_get_sensors_barometer_period() * 1000, true, barometer_timer_cb, b);
   return b;
 }
 
 void barometer_drv_init() {
-  int num_baro=0;
+  int num_baro = 0;
 
-  if ((s_barometer[num_baro] = barometer_create(mgos_i2c_get_global(), 0x76, BARO_BME280)))
+  if ((s_barometer[num_baro] = barometer_create(mgos_i2c_get_global(), 0x76, BARO_BME280))) {
     num_baro++;
+  }
 
-  if ((s_barometer[num_baro] = barometer_create(mgos_i2c_get_global(), 0x60, BARO_MPL115)))
+  if ((s_barometer[num_baro] = barometer_create(mgos_i2c_get_global(), 0x60, BARO_MPL115))) {
     num_baro++;
+  }
 
-  if ((s_barometer[num_baro] = barometer_create(mgos_i2c_get_global(), 0x60, BARO_MPL3115)))
+  if ((s_barometer[num_baro] = barometer_create(mgos_i2c_get_global(), 0x60, BARO_MPL3115))) {
     num_baro++;
+  }
 
-  if ((s_barometer[num_baro] = barometer_create(mgos_i2c_get_global(), 0x77, BARO_MS5611)))
+  if ((s_barometer[num_baro] = barometer_create(mgos_i2c_get_global(), 0x77, BARO_MS5611))) {
     num_baro++;
+  }
 
-  if (num_baro>0)
+  if (num_baro > 0) {
     mgos_prometheus_metrics_add_handler(barometer_prometheus_metrics, NULL);
+  }
 }
 
 #else
